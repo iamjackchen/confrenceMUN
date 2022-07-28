393463673d4f020f663d9dd69e9c064e03b2b541
Jack Chen <35291549+iamjackchen@users.noreply.github.com>
2022-07-28 14:27:19 +0100

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url = `mongodb+srv://iamjackchen:Pass_word129@dev.tbmot.mongodb.net/test?retryWrites=true&w=majority`;
+const url = `mongodb+srv://iamjackchen:INSERTPASSWORDHERE@dev.tbmot.mongodb.net/test?retryWrites=true&w=majority`;
 mongoose.connect(url, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
     .then( () => {
         console.log('Connected to database ')
